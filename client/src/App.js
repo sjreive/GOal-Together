06bb7d0c5046be8cadcb764954b99cf73961358d
Sarah Reive <sarahjreive@gmail.com>
2019-08-30 17:36:44 -0400

    implemented react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,82 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+function App() {
+  return (
+    <Router>
+      <div>
+        <Header />
+
+        <Route exact path="/" component={Home} />
+        <Route path="/commitments" component={Commitments} />
+        <Route path="/notifications" component={Notifications} />
+        <Route path="/newsfeed" component={Newsfeed} />
+        <Route path="/stats" component={Stats} />
+        <Route path="/transactions" component={Transactions} />
       </div>
-    );
-  }
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Commitment({ match }) {
+  return <h2>Commitment ${match.params.id} </h2>;
+}
+
+function Commitments({ match }) {
+  return (
+    <div>
+      <h2>Commitments</h2>
+
+      <ul>
+        <li>
+          <Link to={`${match.url}/new`}>Create a New Commitment</Link>
+        </li>
+        <li>RENDERED COMMITMENTS</li>
+      </ul>
+
+      <Route path={`${match.path}/:id`} component={Commitment} />
+      <Route
+        exact
+        path={match.path}
+        render={() => (
+          <h3>
+            This will render the commitment that is clicked on $
+            {match.params.id}
+          </h3>
+        )}
+      />
+    </div>
+  );
+}
+
+function Notifications() {
+  return <h2>My Notifications</h2>;
+}
+
+function Newsfeed() {
+  return <h2>My Newsfeed</h2>;
+}
+
+function Stats() {
+  return <h2>My Stats</h2>;
+}
+
+function Transactions() {
+  return <h2>My Transactions</h2>;
+}
+
+function Header() {
+  return (
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+    </ul>
+  );
 }
 
 export default App;
