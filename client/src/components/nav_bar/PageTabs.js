c3ff28d24ec77db229e5e24535362ed062b949c3
Kendall Rowe <kendall.rowe312@gmail.com>
2019-09-04 18:24:38 -0400

    Created dumb component logic for tab and pagetabs

---
+++ client/src/components/nav_bar/PageTabs.js
@@ -0,0 +1,19 @@
+import React from "react";
+import classes from "./Nav.module.scss";
+import "./Nav.module.scss";
+
+import Tab from "./Tab";
+
+export default function PageTabs(props) {
+
+  const tabs = props.tabs.map(t => {
+    return <Tab selected={props.mode === t.mode} label={t.label}/>
+  })
+  return (
+    <nav className={classes.tabs}>
+      <ul>
+        {tabs}
+      </ul>
+    </nav>
+  );
+}
---
+++ client/src/components/nav_bar/Tab.js
@@ -0,0 +1,21 @@
+import React from "react";
+import classes from "./Nav.module.scss";
+import "./Nav.module.scss";
+
+const classNames = require('classnames');
+
+export default function Tab(props) {
+  const tabClass = classNames(
+    classes.tab, 
+    props.selected ? classes.selectedTab : null
+  );
+
+  return (
+    <li 
+      classNames={tabClass}
+      onClick={props.onClick}
+    >
+      {props.label}
+    </li>
+  );
+}
