7799bda4856fec6399a42afe775b73d322c08c1b
Kendall Rowe <kendall.rowe312@gmail.com>
2019-09-04 18:48:27 -0400

    Added tabs for commitment page

--- client/src/components/nav_bar/PageTabs.js
+++ client/src/components/nav_bar/PageTabs.js
@@ -6,13 +6,13 @@ import Tab from "./Tab";
 
 export default function PageTabs(props) {
 
-  const tabs = props.tabs.map(t => {
-    return <Tab selected={props.mode === t.mode} label={t.label} onClick={e => props.transition(t.mode)}/>
+  const tabs = props.tabs.map((t, i) => {
+    return <Tab key={i} selected={props.mode === t.mode} label={t.label} onClick={e => props.transition(t.mode)}/>
   });
 
   return (
-    <nav className={classes.tabs}>
-      <ul>
+    <nav className={classes.tabNav}>
+      <ul className={classes.tabs}>
         {tabs}
       </ul>
     </nav>
--- client/src/components/nav_bar/Tab.js
+++ client/src/components/nav_bar/Tab.js
@@ -12,7 +12,7 @@ export default function Tab(props) {
 
   return (
     <li 
-      classNames={tabClass}
+      className={tabClass}
       onClick={props.onClick}
     >
       {props.label}
