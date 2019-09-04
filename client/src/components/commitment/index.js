9db4249222196d996de93672610a2440e64a48c2
Kendall Rowe <kendall.rowe312@gmail.com>
2019-09-04 18:35:49 -0400

    implemented use visual mode for page tabs on commitment page

--- client/src/components/commitment/index.js
+++ client/src/components/commitment/index.js
@@ -3,14 +3,29 @@ import React, { useState, useEffect } from "react";
 // import "./Styles.module.scss";
 import { useVisualMode } from "../../hooks/useVisualMode";
 
+import PageTabs from "../nav_bar/PageTabs";
+
 export default function Commitment(props) {
-  const { mode, transition, back } = useVisualMode("FIRST");
+  const { mode, transition } = useVisualMode("ACTIVITIES");
 
   const [type, setType] = useState(props.type || "");
 
+  const tabs = [
+    {
+      label: "Activities",
+      mode: "ACTIVITIES"
+    },
+    {
+      label: "Stats",
+      mode: "STATS"
+    }
+  ]
+
   return (
     <section >
-      
+      <PageTabs mode={mode} tabs={tabs} transition={transition}/>
+      {mode === "ACTIVITIES" && <h2>Activities</h2>}
+      {mode === "STATS" && <h2>STATS</h2>}
     </section>
   );
 };
--- client/src/components/nav_bar/PageTabs.js
+++ client/src/components/nav_bar/PageTabs.js
@@ -7,8 +7,9 @@ import Tab from "./Tab";
 export default function PageTabs(props) {
 
   const tabs = props.tabs.map(t => {
-    return <Tab selected={props.mode === t.mode} label={t.label}/>
-  })
+    return <Tab selected={props.mode === t.mode} label={t.label} onClick={e => props.transition(t.mode)}/>
+  });
+
   return (
     <nav className={classes.tabs}>
       <ul>
