ac1601d37992502de9c9fb6378233c705ec602f1
Kendall Rowe <kendall.rowe312@gmail.com>
2019-09-09 21:46:35 -0400

    Added basic styling for invitation tile

--- client/src/components/activity/InvitationListItem.js
+++ client/src/components/activity/InvitationListItem.js
@@ -1,29 +1,34 @@
 import React from "react";
+import { Image } from 'cloudinary-react';
+import urls from '../../actions/urls';
 
-import classes from "../commitments/CommitmentListItem.module.scss";
-import MemberList from "../members/MemberList";
+import classes from "./InvitationListItem.module.scss";
 
 export default function InvitationListItem(props) {
   return (
-    <main className={classes.commitmentTile}>
-      <section className={classes.commitmentTile__top}>
+    <article className={classes.commitmentTile}>
+      <div className={classes.commitmentTile__top}>
         <img
           className={classes.commitmentTile__img}
           src={props.thumbnail}
           alt="commitment_category"
         />
-      </section>
-      <section className={classes.commitmentTile__info}>
-        <section className={classes.commitmentTile__txt}>
+        <div className={classes.commitmentTile__txt}>
+          
+          <h2 className={classes.commitmentTile__name}>You've been invited to a commitment!</h2>
+        </div>
+      </div>
+      <div className={classes.commitmentTile__info}>
+        <div className={classes.commitmentTile__txt}>
           <h2 className={classes.commitmentTile__name}>{props.name}</h2>
           <p className={classes.commitmentTile__category}>
             {props.description}
           </p>
-        </section>
-        <section>
+        </div>
+        <div>
           {/* <MemberList members={props.members} /> */}
-        </section>
-      </section>
-    </main>
+        </div>
+      </div>
+    </article>
   );
 }
