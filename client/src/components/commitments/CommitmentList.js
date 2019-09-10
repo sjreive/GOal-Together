f364be24395a1c13d70c31c27dfecf3a7352d8c5
Sarah Reive <sarahjreive@gmail.com>
2019-09-10 18:16:08 -0400

    commitments page filters for members

--- client/src/components/commitments/CommitmentList.js
+++ client/src/components/commitments/CommitmentList.js
@@ -4,7 +4,9 @@ import classes from "./CommitmentList.module.scss";
 import CommitmentListItem from "./CommitmentListItem";
 
 export default function CommitmentList({ Link, match, members, commitments }) {
-  console.log("commitments props:", commitments);
+  console.log("member props:", commitments);
+
+  // const commitment_members = Object.values(members).filter(member => commitment.members.includes(member.id))
 
   const commitmentListItems =
     commitments &&
@@ -16,7 +18,9 @@ export default function CommitmentList({ Link, match, members, commitments }) {
           name={commitments[id].name}
           thumbnail={commitments[id].thumbnail}
           description={commitments[id].description}
-          members={members}
+          members={Object.values(members).filter(member =>
+            commitments[id].members.includes(member.id)
+          )}
         />
       </Link>
     ));
