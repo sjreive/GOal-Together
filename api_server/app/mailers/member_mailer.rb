95160e72fa5b383edbeb616e92ba24e31c3fd5e4
Kendall Rowe <kendall.rowe312@gmail.com>
2019-09-11 12:16:53 -0400

    Added work break for long commitment names on side nav

--- api_server/app/mailers/member_mailer.rb
+++ api_server/app/mailers/member_mailer.rb
@@ -5,7 +5,7 @@ class MemberMailer < ApplicationMailer
     @user = params[:user]
     @commitment = params[:commitment]
     @inviting_user = params[:inviting_user]
-    @url = 'http://example.com/login'
+    @url = 'http://localhost:3000/'
     mail(to: @user.email, subject: 'Welcome to GOal Together!')
   end
 end
