2686315ddc2301b0e7b244dade395bf540a93416
Sarah Reive <sarahjreive@gmail.com>
2019-09-11 19:08:45 -0400

    added names on hover for vote card

--- client/src/components/vote/attendee.js
+++ client/src/components/vote/attendee.js
@@ -35,7 +35,7 @@ export default function VoterListItem(props) {
             icon={faTimesCircle}
           />
         )}
-        <p>{props.name}</p>
+        <p className={classes.members__item_name}>{props.name}</p>
       </div>
     </li>
   );
